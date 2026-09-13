95c55d14ab6ae93ecf72a42f11c2211f949d69bd
alviar <alviar@Alwyn>
2026-09-13 12:04:37 +0800

    Initial commit - basic login

---
+++ src/basiclogin/basiclogin.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package basiclogin;
+
+/**
+ *
+ * @author alviar
+ */
+import java.util.Scanner;
+public class basiclogin {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String user = "Admin";
+        String pass = "admin123";
+        
+        System.out.print("User Name: ");
+        String username = sc.nextLine();
+        System.out.print("Password: ");
+        String password = sc.nextLine();
+        
+        if (username.equals(user) && password.equals(pass)) {
+            System.out.println("Login Succesfully!");
+        }
+        else {
+            System.out.println("Login Unsuccesfully");
+        }   
+    }
+    
+}
