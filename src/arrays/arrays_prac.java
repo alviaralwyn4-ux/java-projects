7933ffece964e6a011efde30314aec6b80b390f1
alviar <alviar@Alwyn>
2026-09-26 23:06:50 +0800

    update code

--- src/arrays/arrays_prac.java
+++ src/arrays/arrays_prac.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package git;
+package arrays;
 
 /**
  *
  * @author alviar
  */
-public class NewClass {
+public class arrays_prac {
     
 }
