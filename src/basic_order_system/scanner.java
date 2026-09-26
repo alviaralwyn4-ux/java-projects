dd2232af6582427220c03d8b7c081b375d51903a
alviar <alviar@Alwyn>
2026-09-26 23:01:07 +0800

    scanner new

---
+++ src/basic_order_system/scanner.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basic_order_system;
+
+/**
+ *
+ * @author alviar
+ */
+import java.util.Scanner;
+public class scanner {
+     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+       
+         
+         
+ 
+    }
+    
+}
